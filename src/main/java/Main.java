06bfb7ed7fba1d344d9eb8c6b951ae032b82bdc8
Ilya.Ulizko <iu@q.ru>
2020-10-07 19:52:37 +0400

    Lesson1. Конфигурирование spring-context.xml и добавление кода в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import model.Client;
+import model.Stallholder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.ServiceStallholder;
@@ -6,7 +8,11 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 
+        Client client = (Client)context.getBean("client1");
+        Stallholder stallholder = (Stallholder)context.getBean("stallHolder1");
         ServiceStallholder serviceStallholder = (ServiceStallholder)context.getBean("serviceStallholder");
-        System.out.println(serviceStallholder.toString());
+        System.out.println(client.toString());
+        System.out.println(stallholder.toString());
+        serviceStallholder.addStallholder(stallholder);
     }
 }
